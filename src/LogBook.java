6570da67f7a51e9de7c12b8b22eddede25776156
dbc2201 <divyanshb22@gmail.com>
2018-12-07 02:10:18 +0530

    add method to add entry

--- src/LogBook.java
+++ src/LogBook.java
@@ -6,9 +6,23 @@
 public class LogBook
 {
 	
+	/*
+	* This constructor creates an empty LogBook for the specified month
+	* i.e., a logbook in which all the entries are zero.
+	* If month is an invalid date, it will default to today's date.
+	* */
 	public LogBook(int month, int year)
 	{
+		//	pre-condition	:	the month must be a valid integer [1, 12] (both inclusive)
+	}
 	
+	/*
+	* This method stores the 'value' as the logbook entry for the specified 'day'.
+	* */
+	void putEntry(int day, int value)
+	{
+		//	pre-condition	:	 day is less than or equal to the number of days
+		//	in the logbook month
 	}
 	
 	public static void main(String[] args)
